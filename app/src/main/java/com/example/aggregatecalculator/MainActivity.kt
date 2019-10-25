f07296706000231c8578a8b02166dc1d4281f0f4
Dave Hannigan <dave.hannigan1@live.co.uk>
2019-10-25 11:14:06 +0100

    First draught

--- app/src/main/java/com/example/aggregatecalculator/MainActivity.kt
+++ app/src/main/java/com/example/aggregatecalculator/MainActivity.kt
@@ -2,11 +2,61 @@ package com.example.aggregatecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateAggregate(view: View){
+        var ourTotal = 0
+        var theirTotal =0
+        val ourScores = arrayOf(findViewById<EditText>(R.id.ourScore1),findViewById<EditText>(R.id.ourScore2),
+            findViewById<EditText>(R.id.ourScore3), findViewById<EditText>(R.id.ourScore4),
+            findViewById<EditText>(R.id.ourScore5), findViewById<EditText>(R.id.ourScore6),
+            findViewById<EditText>(R.id.ourScore7), findViewById<EditText>(R.id.ourScore8))
+
+        val theirScores = arrayOf(findViewById<EditText>(R.id.theirScore1), findViewById<EditText>(R.id.theirScore2),
+            findViewById<EditText>(R.id.theirScore3), findViewById<EditText>(R.id.theirScore4),
+            findViewById<EditText>(R.id.theirScore5), findViewById<EditText>(R.id.theirScore6),
+            findViewById<EditText>(R.id.theirScore7),findViewById<EditText>(R.id.theirScore8))
+
+
+        for(x in ourScores){
+            ourTotal += scoreReturn(x)
+        }
+
+        for(x in theirScores){
+            theirTotal += scoreReturn(x)
+        }
+
+
+        val ourAgg = findViewById<TextView>(R.id.ourAgg)
+        ourAgg.text = ourTotal.toString()
+
+        val theirAgg = findViewById<TextView>(R.id.theirAgg)
+        theirAgg.text = theirTotal.toString()
+
+        val aggResult = findViewById<TextView>(R.id.result)
+        //val finalResult = ourAgg - theirAgg
+        aggResult.text = (ourTotal-theirTotal).toString()
+
+    }
+    fun scoreReturn(editValue: EditText): Int{
+        val score: Int? = editValue.text.toString().toIntOrNull()
+        if(score != null){
+            return score
+        }else{
+            return 0
+        }
+
     }
 }
