5f87d9a5e688de0636dc92b6031cdfee39ec9f5d
Dave Hannigan <dave.hannigan1@live.co.uk>
2020-02-18 08:14:32 +0000

    dashboard added

---
+++ app/src/main/java/com/example/aggregatecalculator/DashboardActivity.kt
@@ -0,0 +1,23 @@
+package com.example.aggregatecalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+
+    fun editTeam(view: View){
+
+    }
+
+    fun enterScores(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
