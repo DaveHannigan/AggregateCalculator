ee542531fcac520db464e8b3c6b9f16d22f528ba
Dave Hannigan <dave.hannigan1@live.co.uk>
2020-04-21 08:43:57 +0100

    end of first phase, opening, saving results

--- app/src/main/java/com/example/aggregatecalculator/EditPlayers.kt
+++ app/src/main/java/com/example/aggregatecalculator/EditPlayers.kt
@@ -3,9 +3,8 @@ package com.example.aggregatecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_edit_players.*
 
 class EditPlayers : AppCompatActivity() {
@@ -20,13 +19,39 @@ class EditPlayers : AppCompatActivity() {
             .also { adapter ->
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
                 spin.adapter = adapter
-                spin.setOnClickListener { getPlayers( ) }
+                spin.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val selectedItem = parent?.getItemAtPosition(position).toString()
+
+                        if(selectedItem == "Please choose a league"){
+                            return
+                        }else{
+                            getPlayers(selectedItem)
+                        }
+
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+
+                }
             }
 
-        buttonGetPlayers.setOnClickListener { getPlayers() }
+            //buttonGetPlayers.setOnClickListener { getPlayers()  }
     }
 
-    fun getPlayers(){
+    fun getPlayers(league: String){
+        val db = FirebaseFirestore.getInstance()
 
+
+
+        Toast.makeText(this, "selected league is $league", Toast.LENGTH_LONG).show()
     }
 }
